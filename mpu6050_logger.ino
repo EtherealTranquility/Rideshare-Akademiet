a9adaf3c73d5822761057a82ba76af2a359d4ca8
daniel <126830112+EtherealTranquility@users.noreply.github.com>
2025-02-07 17:57:24 +0100

    Update mpu6050_logger.ino

--- mpu6050_logger.ino
+++ mpu6050_logger.ino
@@ -6,22 +6,30 @@
 // Opprett objekt for MPU6050
 MPU6050 mpu;
 
-// Definer SD-kort pin
+// Definer SD-kort pin (ikke i bruk akkurat nå)
 const int chipSelect = 4;
 
-void initializeMPU() {
+// Initialiserer MPU6050-sensoren
+void initializeMPU()
+{
     Wire.begin();
     mpu.initialize();
-    if (mpu.testConnection()) {
+    
+    if (mpu.testConnection())
+    {
         Serial.println("MPU6050 tilkoblet!");
-    } else {
+    }
+    else
+    {
         Serial.println("Kunne ikke koble til MPU6050!");
     }
 }
 
 /* SD-kort er ikke tilgjengelig for nå, denne delen er kommentert ut
-bool initializeSD() {
-    if (!SD.begin(chipSelect)) {
+bool initializeSD()
+{
+    if (!SD.begin(chipSelect))
+    {
         Serial.println("SD-kortet kunne ikke initialiseres!");
         return false;
     }
@@ -29,23 +37,32 @@ bool initializeSD() {
     return true;
 }
 
-void createCSVHeader() {
+// Oppretter CSV-header hvis filen er ny
+void createCSVHeader()
+{
     File dataFile = SD.open("sensor_data.csv", FILE_WRITE);
-    if (dataFile) {
+    if (dataFile)
+    {
         dataFile.println("Time(ms),Accel_X,Accel_Y,Accel_Z,Gyro_X,Gyro_Y,Gyro_Z");
         dataFile.close();
-    } else {
+    }
+    else
+    {
         Serial.println("Feil ved åpning av fil!");
     }
 }
 */
 
-void readSensorData(int16_t &ax, int16_t &ay, int16_t &az, int16_t &gx, int16_t &gy, int16_t &gz) {
+// Leser akselerometer- og gyroskopdata fra MPU6050
+void readSensorData(int16_t &ax, int16_t &ay, int16_t &az, int16_t &gx, int16_t &gy, int16_t &gz)
+{
     mpu.getAcceleration(&ax, &ay, &az);
     mpu.getRotation(&gx, &gy, &gz);
 }
 
-void printSensorDataCSV(unsigned long currentTime, int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz) {
+// Skriver sensorverdier til Serial Monitor i CSV-format
+void printSensorDataCSV(unsigned long currentTime, int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz)
+{
     Serial.print(currentTime);
     Serial.print(",");
     Serial.print(ax);
@@ -61,15 +78,19 @@ void printSensorDataCSV(unsigned long currentTime, int16_t ax, int16_t ay, int16
     Serial.println(gz);
 }
 
-void setup() {
-    Serial.begin(115200);
-    initializeMPU();
+void setup()
+{
+    Serial.begin(115200); // Starter seriell kommunikasjon
+    initializeMPU(); // Initialiserer MPU6050
     // if (initializeSD()) { createCSVHeader(); }  // SD-kort init er kommentert ut for nå
 }
 
-void loop() {
-    int16_t ax, ay, az, gx, gy, gz;
-    readSensorData(ax, ay, az, gx, gy, gz);
-    printSensorDataCSV(millis(), ax, ay, az, gx, gy, gz);
-    delay(500);
+void loop()
+{
+    int16_t ax, ay, az, gx, gy, gz; // Variabler for sensorverdier
+    
+    readSensorData(ax, ay, az, gx, gy, gz); // Leser sensorverdier
+    printSensorDataCSV(millis(), ax, ay, az, gx, gy, gz); // Skriver til Serial Monitor i CSV-format
+    
+    delay(500); // Vent 500 ms før neste måling
 }
